bedd8cbbaa21f1dd810ffde519bff90dcd66d2c0
Dev Kumar <devkr@Devs-MacBook-Air.local>
2026-06-29 13:22:29 +0530

    Serve logo image with PNG content type

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ const contentTypes = {
   ".html": "text/html; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".css": "text/css; charset=utf-8",
-  ".json": "application/json; charset=utf-8"
+  ".json": "application/json; charset=utf-8",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".webp": "image/webp",
+  ".svg": "image/svg+xml"
 };
 
 const server = http.createServer(async (req, res) => {
